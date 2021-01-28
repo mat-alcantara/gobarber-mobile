95a08c1824ec521634b78be74093ea54b62ca1ea
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-01-28 07:32:29 -0300

    Input and Button

--- GoStack/gobarber-mobile/src/pages/SingIn/index.tsx
+++ GoStack/gobarber-mobile/src/pages/SingIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -9,6 +12,17 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="Email" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('deu');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
