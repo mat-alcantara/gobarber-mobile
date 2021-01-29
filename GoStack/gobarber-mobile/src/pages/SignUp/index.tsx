b6fc459a46bad08491f12452bf1096ce8d2752cd
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-01-29 06:19:08 -0300

    SignIn Page

--- GoStack/gobarber-mobile/src/pages/SignUp/index.tsx
+++ GoStack/gobarber-mobile/src/pages/SignUp/index.tsx
@@ -1,9 +1,76 @@
+/* eslint-disable no-console */
 import React from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-const SignUp: React.FC = () => {
-  return <Container />;
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  BackToSignIn,
+  BackToSignInText,
+} from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="Email" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log('deu');
+              }}
+            >
+              Entrar
+            </Button>
+            <ForgotPassword
+              onPress={() => {
+                console.log('deu');
+              }}
+            >
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn
+        onPress={() => {
+          console.log('deu');
+        }}
+      >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <BackToSignInText>Criar uma conta</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
-export default SignUp;
+export default SignIn;
